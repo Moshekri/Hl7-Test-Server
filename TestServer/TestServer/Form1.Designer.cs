8d35fe402c3cbcf53666a9b6fd72219736849e6a
Moshekri <moshekri@gmail.com>
2018-11-11 09:32:07 +0200

    Initial commit

---
+++ TestServer/TestServer/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TestServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cmbLocalIp = new System.Windows.Forms.ComboBox();
+            this.btnSaveToFile = new System.Windows.Forms.Button();
+            this.btnQuitProgram = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Listening Port :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Local Listening Ip :";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(212, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(208, 22);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "9010";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(440, 20);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(179, 52);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Server";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(652, 19);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(166, 53);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop   Server";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(912, 356);
+            this.txtLog.TabIndex = 4;
+            this.txtLog.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.txtLog);
+            this.panel1.Location = new System.Drawing.Point(27, 118);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(912, 356);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.cmbLocalIp);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnStop);
+            this.panel2.Controls.Add(this.txtPort);
+            this.panel2.Controls.Add(this.btnStart);
+            this.panel2.Location = new System.Drawing.Point(27, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(912, 85);
+            this.panel2.TabIndex = 6;
+            // 
+            // cmbLocalIp
+            // 
+            this.cmbLocalIp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLocalIp.FormattingEnabled = true;
+            this.cmbLocalIp.Location = new System.Drawing.Point(212, 50);
+            this.cmbLocalIp.Name = "cmbLocalIp";
+            this.cmbLocalIp.Size = new System.Drawing.Size(208, 24);
+            this.cmbLocalIp.TabIndex = 5;
+            // 
+            // btnSaveToFile
+            // 
+            this.btnSaveToFile.Location = new System.Drawing.Point(27, 490);
+            this.btnSaveToFile.Name = "btnSaveToFile";
+            this.btnSaveToFile.Size = new System.Drawing.Size(166, 53);
+            this.btnSaveToFile.TabIndex = 7;
+            this.btnSaveToFile.Text = "Save To File";
+            this.btnSaveToFile.UseVisualStyleBackColor = true;
+            this.btnSaveToFile.Click += new System.EventHandler(this.btnSaveToFile_Click);
+            // 
+            // btnQuitProgram
+            // 
+            this.btnQuitProgram.Location = new System.Drawing.Point(773, 490);
+            this.btnQuitProgram.Name = "btnQuitProgram";
+            this.btnQuitProgram.Size = new System.Drawing.Size(166, 53);
+            this.btnQuitProgram.TabIndex = 7;
+            this.btnQuitProgram.Text = "Quit";
+            this.btnQuitProgram.UseVisualStyleBackColor = true;
+            this.btnQuitProgram.Click += new System.EventHandler(this.btnQuitProgram_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 570);
+            this.Controls.Add(this.btnQuitProgram);
+            this.Controls.Add(this.btnSaveToFile);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Simple TCP Test Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox cmbLocalIp;
+        private System.Windows.Forms.Button btnSaveToFile;
+        private System.Windows.Forms.Button btnQuitProgram;
+    }
+}
+
---
+++ TestServer/TestServer/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace TestServer
+{
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource tokenSource;
+        TcpListener server;
+        System.Timers.Timer timer;
+        IPEndPoint localEndPoint;
+        CancellationToken cancelServer;
+        bool serverStarted = false;
+
+        public Form1()
+        {
+            timer = new System.Timers.Timer(200);
+            InitializeComponent();
+            btnStop.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            var adressess = Dns.GetHostAddresses("localHost");
+            foreach (var add in adressess)
+            {
+                if (add.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    cmbLocalIp.Items.Add(add);
+                    cmbLocalIp.SelectedIndex = 0;
+                }
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            localEndPoint = new IPEndPoint((IPAddress)cmbLocalIp.SelectedItem, int.Parse(txtPort.Text));
+            server = new TcpListener(localEndPoint);
+            tokenSource = new CancellationTokenSource();
+            cancelServer = tokenSource.Token;
+            try
+            {
+                server.Start();
+                serverStarted = true;
+                serverStarted = true;
+                timer.Elapsed += Timer_Elapsed;
+                timer.Start();
+                LogEntry("Server Started ");
+                ChangeButtonState(btnStop, true);
+                ChangeButtonState(btnStart, false);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            byte[] buffer = new byte[50000];
+            StringBuilder sb = new StringBuilder();
+            if (!cancelServer.IsCancellationRequested)
+            {
+                //  LogEntry(server.Pending().ToString());
+                if (server.Pending())
+                {
+                    var client = server.AcceptTcpClient();
+                    var stream = client.GetStream();
+                    if (stream.DataAvailable)
+                    {
+                        int amountRead = 0;
+                        do
+                        {
+                            amountRead = stream.Read(buffer, 0, buffer.Length);
+                            sb.Append(Encoding.ASCII.GetString(buffer));
+                        } while (amountRead == buffer.Length);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+                var message = sb.ToString().TrimEnd(new char[] { '\0' });
+                LogEntry(message);
+            }
+            else
+            {
+                StopServer();
+            }
+        }
+        private void StopServer()
+        {
+
+            server.Stop();
+            serverStarted = false;
+            LogEntry("Server Stopped");
+            timer.Stop();
+            timer.Elapsed -= Timer_Elapsed;
+            ChangeButtonState(btnStop, false);
+            ChangeButtonState(btnStart, true);
+
+
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tokenSource.Cancel();
+
+
+        }
+        private void LogEntry(string text)
+        {
+
+            if (txtLog.InvokeRequired)
+            {
+                txtLog.Invoke(new Action(() => LogEntry(text)));
+            }
+            else
+            {
+                txtLog.AppendText($"{text} {Environment.NewLine}");
+                txtLog.Refresh();
+            }
+
+        }
+        private void ChangeButtonState(Button b, bool state)
+        {
+            if (b.InvokeRequired)
+            {
+                b.Invoke(new Action(() => ChangeButtonState(b, state)));
+            }
+            else
+            {
+                b.Enabled = state;
+            }
+        }
+        public string LongToIP(long longIP)
+        {
+            string ip = string.Empty;
+            for (int i = 0; i < 4; i++)
+            {
+                int num = (int)(longIP / Math.Pow(256, (3 - i)));
+                longIP = longIP - (long)(num * Math.Pow(256, (3 - i)));
+                if (i == 0)
+                    ip = num.ToString();
+                else
+                    ip = ip + "." + num.ToString();
+            }
+            return ip;
+        }
+        private void txtIpAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuitProgram_Click(object sender, EventArgs e)
+        {
+            LogEntry("Checking server status ...");
+            Thread.Sleep(250);
+            if (serverStarted)
+            {
+                LogEntry("Stopping Server ...");
+                Thread.Sleep(500);
+                StopServer();
+                Thread.Sleep(500);
+
+            }
+            else
+            {
+                LogEntry("Server Not running , Exiting ...");
+                Thread.Sleep(250);
+            }
+            LogEntry("Exiting Application ...");
+
+            Thread.Sleep(1500);
+            Application.Exit();
+        }
+
+        private void btnSaveToFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.InitialDirectory = @"\Saves";
+            var result = saveFile.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                File.AppendAllText(saveFile.FileName, txtLog.Text);
+            }
+        }
+    }
+}
